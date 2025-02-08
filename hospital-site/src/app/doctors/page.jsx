b4c01bee8bcf511a7f4fd0266031960e09b567a9
Damien <S10265756@connect.np.edu.sg>
2025-02-08 22:26:14 +0800

    team

--- hospital-site/src/app/doctors/page.jsx
+++ hospital-site/src/app/doctors/page.jsx
@@ -15,8 +15,7 @@ const Team = () => {
     return (
         <>
             <Header /> {/* Add the Header component here */}
-            <div className="min-h-screen bg-white text-gray-800 flex flex-col items-center relative">
-                <DropdownMenu /> {/* Add the DropdownMenu component here */}
+            <div className="min-h-screen bg-white text-gray-800 flex flex-col items-center">
                 <div className="container py-5">
                     <div className="text-center mx-auto mb-5" style={{ maxWidth: "500px" }}>
                         <h5 className="d-inline-block text-secondary text-uppercase border-bottom border-5">Our Doctors</h5>
@@ -62,7 +61,7 @@ const DropdownMenu = () => {
     };
 
     return (
-        <div className="relative z-50">
+        <div className="relative">
             <button className="bg-blue-500 text-white p-2 rounded" onClick={toggleMenu}>Useful Pages</button>
             {isOpen && (
                 <div className="absolute left-0 mt-2 w-48 bg-white shadow-lg z-50">
