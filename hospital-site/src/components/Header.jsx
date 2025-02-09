ca16d98064b55b8e22d53365ecb7974acc5d9f41
Default <s10270276@connect.np.edu.sg>
2025-02-09 09:34:21 +0800

    a

--- hospital-site/src/components/Header.jsx
+++ hospital-site/src/components/Header.jsx
@@ -33,7 +33,7 @@ export default function Header() {
           {isDropdownOpen && (
             <div className="absolute right-0 mt-2 w-48 bg-white rounded-lg shadow-lg z-50">
               <Link href="/" legacyBehavior>
-                <a className="block px-4 py-2 text-blue-900 hover:bg-blue-100">Home</a>
+                <a className="block px-4 py-2 text-blue-900 hover:bg-blue-100">Test Page</a>
               </Link>
               <Link href="/home" legacyBehavior>
                 <a className="block px-4 py-2 text-blue-900 hover:bg-blue-100">Home Page</a>
